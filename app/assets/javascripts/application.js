58592e6991770622e19ff38dd4cde08a380c6613
Damien Le Berrigaud <damien@itsdamo.com>
2012-08-04 20:04:58 -0600

    Added photo gallery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery-1.7.2.min
 //= require jquery-ui-1.8.22.dialog-only.min
+//= require jquery.carousel
 $(function() {
   $('a.modal').on('click', function(event) {
     event && event.preventDefault();
@@ -15,6 +16,18 @@ $(function() {
         $(this).dialog('destroy');
       }
     });
+  });
+
+  $('#gallery').jcarousel();
+
+  $('.previous', '#gallery').click(function(e) {
+    $('#gallery').jcarousel('scroll', '-=1');
+    e.preventDefault();
+  });
+
+  $('.next', '#gallery').click(function(e) {
+    $('#gallery').jcarousel('scroll', '+=1');
+    e.preventDefault();
   });
 
   $('#notice').delay(5000).fadeOut();
