1fb98f08a16aad97b5e036d4e89f2ff81da843f1
Damien Le Berrigaud <damien@itsdamo.com>
2012-08-05 19:37:35 -0600

    Heroku compatible configuration

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -1,1 +1,10 @@
-AUTH_CONFIG = YAML::load_file(Rails.root.join 'config/auth.yml')
+config_file = Rails.root.join('config/auth.yml')
+
+if File.exists? config_file
+  AUTH_CONFIG = YAML::load_file(config_file)
+else
+  AUTH_CONFIG = {
+    'login' => ENV['AUTH_LOGIN'],
+    'password' => ENV['AUTH_PWD']
+  }
+end
